a6a9b09eed19c3523db5e47a4651a3f731063558
Rui Gonçalves <rui@shiftforward.eu>
2013-06-11 12:16:15 +0100

    Fix spaces.

--- src/main/scala/eu/shiftforward/apso/Reflect.scala
+++ src/main/scala/eu/shiftforward/apso/Reflect.scala
@@ -1,9 +1,9 @@
 package eu.shiftforward.apso
 
 object Reflect {
-	def newInstance[T](className: String): T =
-		Class.forName(className).newInstance.asInstanceOf[T]
+  def newInstance[T](className: String): T =
+    Class.forName(className).newInstance.asInstanceOf[T]
 
-	def companion[T](objName: String)(implicit man: Manifest[T]): T = 
-	    Class.forName(objName + "$").getField("MODULE$").get(man.runtimeClass).asInstanceOf[T]
+  def companion[T](objName: String)(implicit man: Manifest[T]): T =
+    Class.forName(objName + "$").getField("MODULE$").get(man.runtimeClass).asInstanceOf[T]
 }
