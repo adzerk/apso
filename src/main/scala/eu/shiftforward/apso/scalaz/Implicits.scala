c1d5a0d461019eb6481fe7ce606e2c5989d10a4a
Andre Silva <andre.beat@gmail.com>
2013-06-13 16:41:13 +0100

    move scalaz implicits so scalaz specific package

---
+++ src/main/scala/eu/shiftforward/apso/scalaz/Implicits.scala
@@ -0,0 +1,15 @@
+package eu.shiftforward.apso.scalaz
+
+import scala.util.{Failure => TFailure, Try, Success => TSuccess}
+import scalaz.{Failure, Success, Validation}
+
+object Implicits {
+  final implicit class ApsoTry[T](val t: Try[T]) extends AnyVal {
+    def toValidation: Validation[Throwable, T] = t
+  }
+
+  implicit def try2validation[T](t: Try[T]): Validation[Throwable, T] = t match {
+    case TSuccess(t) => Success(t)
+    case TFailure(t) => Failure(t)
+  }
+}
