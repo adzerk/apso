312703c4bc88bc33f0fb6f50bfb69ed14f6ad1d0
Rui Gonçalves <rui@shiftforward.eu>
2013-06-18 16:10:00 +0100

    Begin writing scaladoc.

--- src/main/scala/eu/shiftforward/apso/CounterPair.scala
+++ src/main/scala/eu/shiftforward/apso/CounterPair.scala
@@ -1,5 +1,8 @@
 package eu.shiftforward.apso
 
+/**
+  * Constructor and extractor for packing two numbers in an `Int`.
+  */
 object CounterPair {
   @inline def apply(first: Int, second: Int) = (second << 16) | first
   @inline def unapply(data: Int): Option[(Int, Int)] = Some(data & 0xFFFF, data >>> 16)
