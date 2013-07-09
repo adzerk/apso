c4437390b923645a7bcab2760af8a1c25b8e663f
Andre Silva <andre.beat@gmail.com>
2013-07-09 18:45:24 +0100

    Added Logging trait

--- project/Build.scala
+++ project/Build.scala
@@ -37,6 +37,7 @@ object ProjectBuild extends Build {
       "io.spray"                       % "spray-httpx"        % "1.1-M8" % "provided",
       "org.scalaz"                    %% "scalaz-core"        % "7.0.0"  % "provided",
       "org.specs2"                    %% "specs2"             % "1.14"   % "test",
+      "org.slf4j"                      % "slf4j-api"          % "1.7.+",
       "junit"                          % "junit"              % "4.11"   % "test"
     ),
 
---
+++ src/main/scala/eu/shiftforward/apso/Logging.scala
@@ -0,0 +1,14 @@
+package apso
+
+import org.slf4j.LoggerFactory
+
+/**
+ * Trait to mixin a slf4j `Logger` object.
+ */
+trait Logging {
+  /**
+   * The `Logger` object. This logger will have the same name as the concrete class
+   * into which this trait is mixed-in.
+   */
+  val log = LoggerFactory.getLogger(this.getClass.getName)
+}
