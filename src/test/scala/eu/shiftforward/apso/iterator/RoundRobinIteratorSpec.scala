c1c40a6a6c2c25ac6bb61e35f20a856afbd95a3e
Andre Silva <andre.beat@gmail.com>
2013-08-08 15:56:52 +0100

    remove thunks from round robin iterator

--- src/test/scala/eu/shiftforward/apso/iterator/RoundRobinIteratorSpec.scala
+++ src/test/scala/eu/shiftforward/apso/iterator/RoundRobinIteratorSpec.scala
@@ -5,14 +5,14 @@ import org.specs2.mutable._
 class RoundRobinIteratorSpec extends Specification {
   "A RoundRobinIterator" should {
     "take elements from each iterator in order" in {
-      val it = RoundRobinIterator(() => Iterator(1, 2, 3), () => Iterator(1, 2), () => Iterator(1))
+      val it = RoundRobinIterator(Iterator(1, 2, 3), Iterator(1, 2), Iterator(1))
       val el = it.toList
 
       el mustEqual List(1, 1, 1, 2, 2, 3)
     }
 
     "work with empty iterators" in {
-      val it = RoundRobinIterator(() => Iterator(1, 2, 3), () => Iterator(), () => Iterator(1))
+      val it = RoundRobinIterator(Iterator(1, 2, 3), Iterator(), Iterator(1))
       val el = it.toList
 
       el mustEqual List(1, 1, 2, 3)
