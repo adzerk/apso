f0e9f21b2dd79ec5c9c332998420f8c85041a727
Andre Silva <andre.beat@gmail.com>
2013-09-19 13:26:29 +0100

    add pool spec

---
+++ src/test/scala/eu/shiftforward/apso/pool/PoolSpec.scala
@@ -0,0 +1,34 @@
+package eu.shiftforward.apso.pool
+
+import org.specs2.mutable._
+
+class PoolSpec extends Specification {
+  "SimplePool" should {
+    "acquire & release" in {
+      class Foo()
+      val pool = SimplePool(new Foo)
+      val f1 = pool.acquire() // keep the reference
+      pool.release(f1)
+      pool.acquire() must beTheSameAs(f1) // reference equality
+      pool.acquire() must not beTheSameAs (f1)
+    }
+  }
+
+  "SimpleKeyedPool" should {
+    "acquire & release" in {
+      case class Foo(i: Int)
+      val pool = SimpleKeyedPool[Foo, Int](Foo(_), (f, i) => f.i == i)
+      val f1 = pool.acquire(1)
+      pool.release(f1)
+      pool.acquire(1) must beTheSameAs(f1) // reference equality
+      pool.acquire(1) must not beTheSameAs (f1)
+    }
+
+    "respect the key function" in {
+      case class Foo(i: Int)
+      val pool = SimpleKeyedPool[Foo, Int](Foo(_), (f, i) => f.i == i)
+      val f1 = pool.acquire(1)
+      f1.i === 1
+    }
+  }
+}
