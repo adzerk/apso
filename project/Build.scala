436f74866e010df9726fe2afd561e192b83ac07d
Rui Gonçalves <rui@shiftforward.eu>
2013-10-30 17:14:09 +0000

    Bump to version 0.3-SNAPSHOT for development.

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object ProjectBuild extends Build {
                           settings = Project.defaultSettings ++ formatSettings)
                             .settings(
     organization := "eu.shiftforward",
-    version := "0.2.1",
+    version := "0.3-SNAPSHOT",
     scalaVersion := "2.10.3",
 
     publishSetting,
