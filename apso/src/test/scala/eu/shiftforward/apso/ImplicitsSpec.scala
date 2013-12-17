ff8ffe7378b4a628891e1d6eed0eeef3abc47899
Rui Gonçalves <rui@shiftforward.eu>
2013-12-17 09:55:45 +0000

    Remove duplicate spec example.

--- apso/src/test/scala/eu/shiftforward/apso/ImplicitsSpec.scala
+++ apso/src/test/scala/eu/shiftforward/apso/ImplicitsSpec.scala
@@ -28,19 +28,6 @@ class ImplicitsSpec extends Specification {
     "throw an exception when the average is called on an empty traversable" in {
       List[Int]().average must throwA[IllegalArgumentException]
     }
-
-    "convert correctly a list of maps into a map of lists with a zero value" in {
-      List[Map[Int, Int]]().sequenceOnMap(Some(0)) === Map[Int, List[Int]]()
-
-      List(Map(1 -> 2), Map(1 -> 3), Map(2 -> 3)).sequenceOnMap(Some(0)) ===
-        Map(1 -> List(2, 3, 0), 2 -> List(0, 0, 3))
-
-      List(Map(1 -> "c", 2 -> "b", 3 -> "a")).sequenceOnMap(Some("")) ===
-        Map(1 -> List("c"), 2 -> List("b"), 3 -> List("a"))
-
-      List(Map(1 -> "c", 2 -> "b", 3 -> "a"), Map(1 -> "c2", 4 -> "aa")).sequenceOnMap(Some("")) ===
-        Map(1 -> List("c", "c2"), 2 -> List("b", ""), 3 -> List("a", ""), 4 -> List("", "aa"))
-    }
   }
 
   "An ApsoListMap" should {
