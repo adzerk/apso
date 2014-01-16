eafebf9fe1e1188bbda3888446bdc80ca8ad7aac
Hugo Ferreira <hugo@shiftforward.eu>
2014-01-16 01:11:44 +0000

    Improved instrumentation and tests of the memoization decorator.

--- apso/src/main/scala/eu/shiftforward/apso/Memo.scala
+++ apso/src/main/scala/eu/shiftforward/apso/Memo.scala
@@ -10,6 +10,11 @@ import scala.collection.mutable.HashMap
  */
 trait Memoization[-In, +Out] {
   protected[this] val cache = HashMap.empty[In, Out]
+
+  /**
+   * Un-memoizes all previously memoized results.
+   */
+  def clear() { cache.clear() }
 }
 
 /**
@@ -18,17 +23,32 @@ trait Memoization[-In, +Out] {
  * @tparam In The (possibly product) type of the function that is memoized.
  * @tparam Out The type of the result.
  */
-
 trait MemoizationStats[-In, +Out] extends Memoization[In, Out] {
+  private[this] var cacheHit: Int = 0
+  private[this] var cacheMiss: Int = 0
+
+  protected[this] override val cache = new HashMap[In, Out] {
+    override def getOrElseUpdate(key: In, op: => Out): Out =
+      get(key) match {
+        case Some(v) => cacheHit += 1; v
+        case None => val d = op; this(key) = d; cacheMiss += 1; d
+      }
+  }
+
   /**
-   * Returns the size of the cache.
+   * Returns how many results are currently memoized.
    */
   def size = cache.size
 
   /**
-   * Clears all memoized results.
+   * Returns the number of times a memoized result was used instead of invoking the function.
    */
-  def clear() = cache.clear()
+  def hits = cacheHit
+
+  /**
+   * Returns the number of times a new result was calculated.
+   */
+  def misses = cacheMiss
 }
 
 /**
--- apso/src/test/scala/eu/shiftforward/apso/MemoSpec.scala
+++ apso/src/test/scala/eu/shiftforward/apso/MemoSpec.scala
@@ -3,26 +3,32 @@ package eu.shiftforward.apso
 import org.specs2.mutable._
 
 class MemoizationSpec extends Specification {
-  "A memoiziation decorator" should {
+  "A memoization decorator" should {
     "return the value of the function when called" in {
       val power = Memo { n: Int => n * n }
       power(2) === 4
       power(3) === 9
     }
 
-    "don't grow with multiple invocations with the same parameters" in {
+    "cache once and return the cached result after the first invocation" in {
       val power = new Memo({ n: Int => n * n }) with MemoizationStats[Int, Int]
       power.size === 0
-      (1 to 100).foreach { i => power(i) }
+      (1 to 100).foreach { i => power(i); power(i) }
       power.size === 100
+      power.misses === 100
+      power.hits === 100
     }
 
     "clean when mandated to" in {
       val power = new Memo({ n: Int => n * n }) with MemoizationStats[Int, Int]
-      power(2) === 4
+      power(2)
       power.size === 1
+      power.misses === 1
       power.clear()
       power.size === 0
+      power(2)
+      power.size === 1
+      power.misses === 2
     }
   }
 }
