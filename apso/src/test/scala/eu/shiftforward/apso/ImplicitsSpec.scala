ec729577a2f566484a5aa8e43f4597f44a0e7c16
Rui Gonçalves <rui@shiftforward.eu>
2014-04-17 16:21:24 +0100

    Add alias method and method for naively choose a random element from a sequence.

--- apso/src/test/scala/eu/shiftforward/apso/ImplicitsSpec.scala
+++ apso/src/test/scala/eu/shiftforward/apso/ImplicitsSpec.scala
@@ -116,45 +116,82 @@ class ImplicitsSpec extends Specification {
 
     class MockRandom(elems: Double*) extends Random {
       private[this] var nextElems = elems.toList
-      override def nextDouble(): Double = { val e = nextElems.head; nextElems = nextElems.tail; e }
+      override def nextInt() = { val e = nextElems.head.toInt; nextElems = nextElems.tail; e }
+      override def nextInt(n: Int) = { val e = nextElems.head.toInt % n; nextElems = nextElems.tail; e }
+      override def nextDouble() = { val e = nextElems.head; nextElems = nextElems.tail; e }
+    }
+
+    "select correctly an element from a sequence" in {
+      val rand = new MockRandom(1, 4, 3, 2)
+      val seq = Vector("a", "b", "c", "d", "e")
+
+      rand.choose(seq) === Some("b")
+      rand.choose(seq) === Some("e")
+      rand.choose(seq) === Some("d")
+      rand.choose(Vector()) === None
+    }
+
+    "select correctly an element from a sequence using weights" in {
+      val rand = new MockRandom(0.2, 0.8, 0.7, 0.1)
+      val map = Map("a" -> 0.5, "b" -> 0.25, "c" -> 0.25)
+
+      rand.monteCarlo(map) === Some("a")
+      rand.monteCarlo(map) === Some("c")
+      rand.monteCarlo(map) === Some("b")
+      rand.monteCarlo(Map.empty[String, Double]) === None
     }
 
     "select correctly an element from a sequence using weights" in {
       val rand = new MockRandom(0.2, 0.8, 0.7)
-      val list = List(("a", 0.5), ("b", 0.25), ("c", 0.25))
+      val map = Map("a" -> 0.5, "b" -> 0.25, "c" -> 0.25)
 
-      rand.weightedChoice[(String, Double)](list, _._2) === Some("a", 0.5)
-      rand.weightedChoice[(String, Double)](list, _._2) === Some("c", 0.25)
-      rand.weightedChoice[(String, Double)](list, _._2) === Some("b", 0.25)
+      rand.monteCarlo(map) === Some("a")
+      rand.monteCarlo(map) === Some("c")
+      rand.monteCarlo(map) === Some("b")
     }
 
     "select correctly an element from a sequence in case of underweighting" in {
       val rand = new MockRandom(0.2, 0.8, 0.7)
-      val list = List(("a", 0.5), ("b", 0.25))
+      val map = Map("a" -> 0.5, "b" -> 0.25)
 
-      rand.weightedChoice[(String, Double)](list, _._2) === Some("a", 0.5)
-      rand.weightedChoice[(String, Double)](list, _._2) === None
-      rand.weightedChoice[(String, Double)](list, _._2) === Some("b", 0.25)
+      rand.monteCarlo(map) === Some("a")
+      rand.monteCarlo(map) === None
+      rand.monteCarlo(map) === Some("b")
     }
 
     "select an element from a sequence with a custom weight range" in {
       val rand = new MockRandom(0.2, 0.8, 0.7)
-      val list = List(("a", 0.5), ("b", 0.5), ("c", 0.5))
+      val map = Map("a" -> 0.5, "b" -> 0.5, "c" -> 0.5)
 
-      rand.weightedChoice[(String, Double)](list, _._2, rand.nextDouble() * 1.5) === Some("a", 0.5)
-      rand.weightedChoice[(String, Double)](list, _._2, rand.nextDouble() * 1.5) === Some("c", 0.5)
-      rand.weightedChoice[(String, Double)](list, _._2, rand.nextDouble() * 1.5) === Some("c", 0.5)
+      rand.monteCarlo(map, rand.nextDouble() * 1.5) === Some("a")
+      rand.monteCarlo(map, rand.nextDouble() * 1.5) === Some("c")
+      rand.monteCarlo(map, rand.nextDouble() * 1.5) === Some("c")
     }
 
     "select a random element using reservoir sampling" in {
       val rand = new MockRandom(0.4, 0.2, 0.8, 0.3, 0.7, 0.3, 0.9, 0.45, 0.1, 0.35, 0.9, 0.9)
       val list = List("a", "b", "c")
 
-      rand.reservoirSample(Nil) === None
       rand.reservoirSample(list) === Some("b") // 0.2 < 1/2 in (a, b), 0.8 > 1/3 in (b, c)
       rand.reservoirSample(list) === Some("c") // 0.7 > 1/2 in (a, b), 0.3 < 1/3 in (a, c)
       rand.reservoirSample(list) === Some("c") // 0.45 < 1/2 in (a, b), 0.1 < 1/3 in (b, c)
       rand.reservoirSample(list) === Some("a") // 0.9 > 1/2 in (a, b), 0.9 > 1/3 in (a, c)
+      rand.reservoirSample(Nil) === None
+    }
+
+    "provide a stream of samples with a given distribution" in {
+      val rand = Random
+      val map = Map("a" -> 0.2, "b" -> 0.3, "c" -> 0.5)
+      val runs = 10000
+
+      rand.samples(Map.empty[String, Double]) must beEmpty
+
+      val sampleDistr = rand.samples(map).take(runs).
+        foldLeft(Map.empty[String, Int]) { case (acc, k) => acc.updated(k, acc.getOrElse(k, 0) + 1) }
+
+      forall(map) {
+        case (k, prob) => sampleDistr(k).toDouble must beCloseTo(runs * prob, runs * prob * 0.1)
+      }
     }
   }
 }
