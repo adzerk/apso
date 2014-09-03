a4ab585426b8f59ecf6e6319e26737f5564eb635
Rui Gonçalves <ruippeixotog@gmail.com>
2014-09-03 17:13:24 +0100

    Add a simple, synchronous HTTP client

---
+++ apso-testkit/src/main/scala/eu/shiftforward/apso/http/W.scala
@@ -0,0 +1,25 @@
+package eu.shiftforward.apso.http
+
+import com.ning.http.client.Response
+import dispatch.Defaults._
+import dispatch._
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+object W {
+  private[this] lazy val log = LoggerFactory.getLogger("W")
+
+  implicit def stringAsReq(str: String) = url(str)
+
+  def exec(req: Req) = {
+    log.debug("{} {}", req.toRequest.getMethod, req.toRequest.getUrl, null)
+    Await.result(Http(req), 3.seconds)
+  }
+
+  def get(req: String): Response = exec(req.GET)
+  def post(req: String, body: String): Response = exec(req.POST.setBody(body))
+  def put(req: String, body: String): Response = exec(req.PUT.setBody(body))
+  def delete(req: String): Response = exec(req.DELETE)
+}
