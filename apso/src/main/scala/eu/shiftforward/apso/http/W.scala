4da20f0f4c17ff981b4349225e7f66503c381283
Bruno Maia <bruno.mm.maia@gmail.com>
2014-11-28 12:06:46 +0000

    Use scoped timeout class as implicit

--- apso/src/main/scala/eu/shiftforward/apso/http/W.scala
+++ apso/src/main/scala/eu/shiftforward/apso/http/W.scala
@@ -9,8 +9,9 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 
 object W {
+  case class Timeout(duration: FiniteDuration)
   private[this] lazy val log = LoggerFactory.getLogger("W")
-  private[this] lazy val defaultTimeout = 10.seconds
+  private[this] lazy val defaultTimeout = Timeout(10.seconds)
 
   implicit def stringAsReq(str: String) = url(str)
 
@@ -19,14 +20,14 @@ object W {
     Await.result(Http(req), timeout)
   }
 
-  def get(req: String, headers: Map[String, Seq[String]] = Map())(implicit timeout: FiniteDuration = defaultTimeout): Response =
-    exec(req.GET.setHeaders(headers), timeout)
-  def post(req: String, body: String, headers: Map[String, Seq[String]] = Map())(implicit timeout: FiniteDuration = defaultTimeout): Response =
-    exec(req.POST.setBody(body).setHeaders(headers), timeout)
-  def put(req: String, body: String, headers: Map[String, Seq[String]] = Map())(implicit timeout: FiniteDuration = defaultTimeout): Response =
-    exec(req.PUT.setBody(body).setHeaders(headers), timeout)
-  def delete(req: String, headers: Map[String, Seq[String]] = Map())(implicit timeout: FiniteDuration = defaultTimeout): Response =
-    exec(req.DELETE.setHeaders(headers), timeout)
-  def head(req: String, headers: Map[String, Seq[String]] = Map())(implicit timeout: FiniteDuration = defaultTimeout): Response =
-    exec(req.HEAD.setHeaders(headers), timeout)
+  def get(req: String, headers: Map[String, Seq[String]] = Map())(implicit timeout: Timeout = defaultTimeout): Response =
+    exec(req.GET.setHeaders(headers), timeout.duration)
+  def post(req: String, body: String, headers: Map[String, Seq[String]] = Map())(implicit timeout: Timeout = defaultTimeout): Response =
+    exec(req.POST.setBody(body).setHeaders(headers), timeout.duration)
+  def put(req: String, body: String, headers: Map[String, Seq[String]] = Map())(implicit timeout: Timeout = defaultTimeout): Response =
+    exec(req.PUT.setBody(body).setHeaders(headers), timeout.duration)
+  def delete(req: String, headers: Map[String, Seq[String]] = Map())(implicit timeout: Timeout = defaultTimeout): Response =
+    exec(req.DELETE.setHeaders(headers), timeout.duration)
+  def head(req: String, headers: Map[String, Seq[String]] = Map())(implicit timeout: Timeout = defaultTimeout): Response =
+    exec(req.HEAD.setHeaders(headers), timeout.duration)
 }
