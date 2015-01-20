938230ef2b6a2f9783ec946d0823b3243e723bbd
Andre Silva <andre.beat@gmail.com>
2015-01-20 17:14:06 +0000

    add strict logging trait

--- apso/src/main/scala/eu/shiftforward/apso/Logging.scala
+++ apso/src/main/scala/eu/shiftforward/apso/Logging.scala
@@ -3,12 +3,23 @@ package eu.shiftforward.apso
 import org.slf4j.LoggerFactory
 
 /**
- * Trait to mixin a slf4j `Logger` object.
+ * Trait to mixin a slf4j `Logger` object. The `Logger` object is initialized lazily.
  */
 trait Logging {
   /**
    * The `Logger` object. This logger will have the same name as the concrete class
    * into which this trait is mixed-in.
    */
-  lazy val log = LoggerFactory.getLogger(this.getClass.getName)
+  lazy val log = LoggerFactory.getLogger(getClass.getName)
+}
+
+/**
+ * Trait to mixin a slf4j `Logger` object. The `Logger` object is initialized strictly.
+ */
+trait StrictLogging {
+  /**
+   * The `Logger` object. This logger will have the same name as the concrete class
+   * into which this trait is mixed-in.
+   */
+  val log = LoggerFactory.getLogger(getClass.getName)
 }
