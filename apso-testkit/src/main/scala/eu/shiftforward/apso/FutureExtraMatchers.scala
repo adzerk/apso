e83f055e09a7abfae5467227c02b6886d54c3fb4
Hugo Ferreira <hugo@shiftforward.eu>
2015-05-29 12:00:51 +0100

    Upgrade Specs2 2.x to 3.x. Bump apso version to 0.6-SNAPSHOT. Other updates.

--- apso-testkit/src/main/scala/eu/shiftforward/apso/FutureExtraMatchers.scala
+++ apso-testkit/src/main/scala/eu/shiftforward/apso/FutureExtraMatchers.scala
@@ -2,12 +2,11 @@ package eu.shiftforward.apso
 
 import org.specs2.matcher._
 import org.specs2.mutable.SpecificationLike
-import org.specs2.time.NoTimeConversions
 
 import scala.concurrent._
 import scala.concurrent.duration._
 
-trait FutureExtraMatchers extends NoTimeConversions { this: SpecificationLike =>
+trait FutureExtraMatchers { this: SpecificationLike =>
 
   implicit class RichAwaitable[T](val awaitable: Awaitable[T]) {
     def get = Await.result(awaitable, 1.second)
@@ -18,7 +17,7 @@ trait FutureExtraMatchers extends NoTimeConversions { this: SpecificationLike =>
     /**
      * @return a matcher that needs to eventually match, after a given number of retries.
      */
-    def eventually(retries: Int): Matcher[T] = EventuallyMatchers.eventually(m, retries, 100.milliseconds)
+    def eventually(retries: Int): Matcher[T] = EventuallyMatchers.eventually(retries, 100.milliseconds)(m)
   }
 
   def beEventually[T](f: T => MatchResult[_]): Matcher[T] =
