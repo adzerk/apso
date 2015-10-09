31434e8344f2503ac0fd6878eb5e7e3b5f8e9095
Andre Silva <andre.beat@gmail.com>
2015-10-09 12:03:26 +0100

    add object for Geo-location utilities

---
+++ apso/src/main/scala/eu/shiftforward/apso/Geo.scala
@@ -0,0 +1,32 @@
+package eu.shiftforward.apso
+
+import scala.math.{ abs, acos, cos, sin, toRadians }
+
+/**
+ * Object containing geo-location functions.
+ */
+object Geo {
+  /**
+   * A pair of `Double` representing, respectively, the latitude and longitude of a position.
+   */
+  type Coordinate = (Double, Double)
+
+  private[this] val EARTH_RADIUS = 6371.009 // kilometers
+
+  /**
+   * Returns the distance in kilometers between two points on the planet Earth, calculated by the
+   * spherical law of cosines (https://en.wikipedia.org/wiki/Great-circle_distance#Formulas).
+   * @param p1 the coordinates of the first point
+   * @param p2 the coordinates of the second point
+   * @return the distance in kilometers between two points on the planet Earth.
+   */
+  def distance(p1: Coordinate, p2: Coordinate) = {
+    val deltaLong = toRadians(abs(p1._2 - p2._2))
+    val lat1 = toRadians(p1._1)
+    val lat2 = toRadians(p2._1)
+
+    val c = acos((sin(lat1) * sin(lat2)) + (cos(lat1) * cos(lat2) * cos(deltaLong)))
+
+    EARTH_RADIUS * c
+  }
+}
