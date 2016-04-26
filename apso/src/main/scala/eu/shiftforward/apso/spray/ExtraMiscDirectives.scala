f6e37b9cd1c02052f9a304bbbe2dd76b9bfffca5
Luís Fonseca <luis@shiftforward.eu>
2016-04-26 17:44:40 +0100

    Implement a directive to extract optionally the referer host

--- apso/src/main/scala/eu/shiftforward/apso/spray/ExtraMiscDirectives.scala
+++ apso/src/main/scala/eu/shiftforward/apso/spray/ExtraMiscDirectives.scala
@@ -1,9 +1,14 @@
 package eu.shiftforward.apso.spray
 
+import java.net.URL
+
+import scala.util.Try
+
 import spray.http.CacheDirectives.{ `max-age`, `must-revalidate`, `no-cache`, `no-store` }
 import spray.http.HttpHeaders.`Cache-Control`
 import spray.routing._
 import spray.routing.directives.RespondWithDirectives
+import spray.routing.directives.HeaderDirectives._
 
 /**
  * Exposes additional misc directives not present in [[spray.routing.directives.MiscDirectives]].
@@ -17,6 +22,13 @@ trait ExtraMiscDirectives {
       case Some(s) =>
         RespondWithDirectives.respondWithSingletonHeader(`Cache-Control`(`max-age`(60l * s), `must-revalidate`))
     }
+
+  /**
+   * Extracts a valid Referer host from the HTTP request headers
+   */
+  def optionalRefererHost: Directive1[Option[String]] =
+    optionalHeaderValueByName("referer")
+      .map(_.flatMap(r => Try(new URL(r)).toOption.map(_.getHost)))
 }
 
 object ExtraMiscDirectives extends ExtraMiscDirectives {
