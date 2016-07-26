e5080438d4144ee45e9176aff2b8700c554265e5
Bruno Maia <bruno.mm.maia@gmail.com>
2016-07-26 16:32:28 +0100

    Revert to old behaviour but parse the x-forward-for header manually

--- apso/src/main/scala/eu/shiftforward/apso/spray/ClientIPDirectives.scala
+++ apso/src/main/scala/eu/shiftforward/apso/spray/ClientIPDirectives.scala
@@ -2,12 +2,12 @@ package eu.shiftforward.apso.spray
 
 import spray.http.RemoteAddress
 import spray.routing.Directive1
-import spray.routing.directives.{ BasicDirectives, MiscDirectives }
+import spray.routing.directives.{ BasicDirectives, HeaderDirectives, MiscDirectives }
 
 /**
  * Directives to extract the value of the Client IP.
  */
-trait ClientIPDirectives extends BasicDirectives with MiscDirectives {
+trait ClientIPDirectives extends BasicDirectives with HeaderDirectives with MiscDirectives {
 
   /**
    * Directive extracting the RemoteAddress of the client from either the X-Forwarded-For, Remote-Address or X-Real-IP header
@@ -23,5 +23,10 @@ trait ClientIPDirectives extends BasicDirectives with MiscDirectives {
    * (in that order of priority).
    */
   def optionalRawClientIP: Directive1[Option[String]] =
-    optionalClientIP.map(_.map(_.value))
+    // This isn't implemented using spray's `clientIp` directive since it is not guaranteed that we
+    // have a valid `RemoteAddress` as the header value (e.g. due to IP anonymization).
+    headerValuePF { case h if h.is("x-forwarded-for") => h.value.split(",").headOption } |
+      headerValuePF { case h if h.is("remote-address") => Option(h.value) } |
+      headerValuePF { case h if h.is("x-real-ip") => Option(h.value) } |
+      provide(None)
 }
