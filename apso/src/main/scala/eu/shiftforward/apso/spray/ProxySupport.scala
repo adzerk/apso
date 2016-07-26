354cfc7b73937f2618be2b75dd64571624f8b791
Bruno Maia <bruno.mm.maia@gmail.com>
2016-07-26 15:59:31 +0100

    Fix adding of `X-Forwarded-For` in ProxySupport trait

--- apso/src/main/scala/eu/shiftforward/apso/spray/ProxySupport.scala
+++ apso/src/main/scala/eu/shiftforward/apso/spray/ProxySupport.scala
@@ -3,9 +3,10 @@ package eu.shiftforward.apso.spray
 import akka.actor.ActorSystem
 import akka.io.IO
 import spray.can.Http
-import spray.http.{ HttpHeader, HttpRequest, Uri }
-import spray.http.HttpHeaders.`X-Forwarded-For`
-import spray.routing.{ RequestContext, Route }
+import spray.http.{ HttpHeader, HttpRequest, RemoteAddress, Uri }
+import spray.http.HttpHeaders.{ `Remote-Address`, `X-Forwarded-For` }
+import spray.routing.directives.HeaderDirectives._
+import spray.routing.{ Directive1, RequestContext, Route }
 
 /**
  * Allows to proxy a request to another URI.
@@ -29,15 +30,32 @@ trait ProxySupport extends ClientIPDirectives {
     ctx => transport.tell(f(ctx), ctx.responder)
   }
 
-  private def getHeaders(ip: Option[String], headers: List[HttpHeader] = Nil) = {
+  private def getHeaders(ip: Option[RemoteAddress], headers: List[HttpHeader] = Nil) = {
     // filter `Host` header
     val hs = headers.filterNot(header => header.is("host"))
     // add `X-Forwarded-For` header
-    ip.fold(hs)(`X-Forwarded-For`(_) :: hs)
+    ip.fold(hs)(addForwardedFor(_, hs))
   }
 
+  private def addForwardedFor(ip: RemoteAddress, headers: List[HttpHeader]): List[HttpHeader] = {
+    headers match {
+      case Nil =>
+        // No `X-Forwarded-For` found in headers, so just add the new one
+        `X-Forwarded-For`(ip) :: Nil
+
+      case `X-Forwarded-For`(ips) :: tail =>
+        `X-Forwarded-For`(ips :+ ip) :: tail
+
+      case notForwardedFor :: tail =>
+        notForwardedFor :: addForwardedFor(ip, tail)
+    }
+  }
+
+  private val optionalRemoteAddress: Directive1[Option[RemoteAddress]] =
+    headerValuePF { case `Remote-Address`(address) ⇒ Some(address) } | provide(None)
+
   def proxyTo(uri: Uri)(implicit system: ActorSystem): Route = {
-    optionalRawClientIP { ip =>
+    optionalRemoteAddress { ip =>
       sending(ctx => ctx.request.copy(
         uri = uri,
         headers = getHeaders(ip, ctx.request.headers)))
@@ -45,7 +63,7 @@ trait ProxySupport extends ClientIPDirectives {
   }
 
   def proxyToUnmatchedPath(uri: Uri)(implicit system: ActorSystem): Route = {
-    optionalRawClientIP { ip =>
+    optionalRemoteAddress { ip =>
       sending { ctx =>
         ctx.request.copy(
           uri = uri.withPath(uri.path.++(ctx.unmatchedPath)).withQuery(ctx.request.uri.query),
