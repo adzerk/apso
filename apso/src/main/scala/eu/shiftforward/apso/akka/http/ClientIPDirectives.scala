14279a1d1a6b057416b04a2d99e02d2bb4d6b7cb
Rui Gonçalves <ruippeixotog@gmail.com>
2016-09-27 14:34:06 +0100

    Ports the Spray-based reverse proxy routes to akka-http

---
+++ apso/src/main/scala/eu/shiftforward/apso/akka/http/ClientIPDirectives.scala
@@ -0,0 +1,32 @@
+package eu.shiftforward.apso.akka.http
+
+import akka.http.scaladsl.model.RemoteAddress
+import akka.http.scaladsl.server.Directive1
+import akka.http.scaladsl.server.Directives._
+
+/**
+ * Directives to extract the value of the clients' IP addresses.
+ */
+trait ClientIPDirectives {
+
+  /**
+   * Directive extracting the remote address of the client from the `X-Forwarded-For`, `Remote-Address` or `X-Real-IP`
+   * headers (in that order).
+   */
+  def optionalClientIP: Directive1[Option[RemoteAddress]] =
+    extractClientIP.map(Option(_)).recoverPF {
+      case Nil => provide(None)
+    }
+
+  /**
+   * Directive extracting the raw IP of the client from either the `X-Forwarded-For`, `Remote-Address` or `X-Real-IP`
+   * headers (in that order).
+   */
+  // This isn't implemented using spray's `clientIp` directive since it is not guaranteed that we
+  // have a valid `RemoteAddress` as the header value (e.g. due to IP anonymization).
+  def optionalRawClientIP: Directive1[Option[String]] =
+    headerValuePF { case h if h.is("x-forwarded-for") => h.value.split(",").headOption } |
+      headerValuePF { case h if h.is("remote-address") => Option(h.value) } |
+      headerValuePF { case h if h.is("x-real-ip") => Option(h.value) } |
+      provide(None)
+}
--- project/Build.scala
+++ project/Build.scala
@@ -26,7 +26,8 @@ object ProjectBuild extends Build {
       "com.jcraft"                     % "jzlib"                     % "1.1.3",
       "com.twmacinta"                  % "fast-md5"                  % "2.7.1",
       "com.typesafe"                   % "config"                    % "1.3.0"          % "provided",
-      "com.typesafe.akka"             %% "akka-actor"                % "2.4.8"          % "provided",
+      "com.typesafe.akka"             %% "akka-actor"                % "2.4.10"         % "provided",
+      "com.typesafe.akka"             %% "akka-http-experimental"    % "2.4.10"         % "provided",
       "io.github.andrebeat"           %% "scala-pool"                % "0.3.0",
       "io.spray"                      %% "spray-can"                 % "1.3.3"          % "provided",
       "io.spray"                      %% "spray-httpx"               % "1.3.3"          % "provided",
@@ -37,19 +38,21 @@ object ProjectBuild extends Build {
       "org.bouncycastle"               % "bcprov-jdk15on"            % "1.54",
       "org.scalaz"                    %% "scalaz-core"               % "7.2.5"          % "provided",
       "org.slf4j"                      % "slf4j-api"                 % "1.7.21",
+      "com.typesafe.akka"             %% "akka-http-testkit"         % "2.4.10"         % "test",
       "io.spray"                      %% "spray-testkit"             % "1.3.3"          % "test",
+      "junit"                          % "junit"                     % "4.12"           % "test",
+      "org.scalacheck"                %% "scalacheck"                % "1.13.2"         % "test",
       "org.specs2"                    %% "specs2-core"               % "3.8.4"          % "test",
       "org.specs2"                    %% "specs2-scalacheck"         % "3.8.4"          % "test",
-      "org.specs2"                    %% "specs2-junit"              % "3.8.4"          % "test",
-      "junit"                          % "junit"                     % "4.12"           % "test",
-      "org.scalacheck"                %% "scalacheck"                % "1.13.2"         % "test"))
+      "org.specs2"                    %% "specs2-junit"              % "3.8.4"          % "test"))
 
   lazy val apsoTestkit = Project("apso-testkit", file("apso-testkit"))
     .settings(commonSettings: _*)
     .settings(publishSettings: _*)
     .settings(apsoTestkitSettings: _*)
     .settings(libraryDependencies ++= Seq(
-      "com.typesafe.akka"             %% "akka-testkit"       % "2.4.8"          % "provided",
+      "com.typesafe.akka"             %% "akka-testkit"       % "2.4.10"         % "provided",
+      "com.typesafe.akka"             %% "akka-http-testkit"  % "2.4.10"         % "provided",
       "org.slf4j"                      % "slf4j-api"          % "1.7.21",
       "org.specs2"                    %% "specs2-core"        % "3.8.4"          % "provided",
       "org.specs2"                    %% "specs2-junit"       % "3.8.4"          % "provided"
