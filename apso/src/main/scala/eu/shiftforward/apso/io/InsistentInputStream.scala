93b2a210656d7b4adefb3fdd23e9aceb268222be
João Costa <joao.costa@shiftforward.eu>
2016-10-18 13:03:06 +0100

    Improve InsistentInputStream bulk read performance

--- apso/src/main/scala/eu/shiftforward/apso/io/InsistentInputStream.scala
+++ apso/src/main/scala/eu/shiftforward/apso/io/InsistentInputStream.scala
@@ -6,6 +6,8 @@ import scala.annotation.tailrec
 import scala.concurrent.duration._
 import scala.util.{ Failure, Success, Try }
 
+import eu.shiftforward.apso.Logging
+
 /**
  * A InputStream that wraps another InputStream, retrying failed reads. This is useful for input streams that can have
  * transient failures (eg HTTP input streams).
@@ -14,7 +16,7 @@ import scala.util.{ Failure, Success, Try }
  * @param maxRetries maximum number of times to retry a read
  * @param backoff optional duration to wait between retries
  */
-class InsistentInputStream(streamBuilder: () => InputStream, maxRetries: Int = 10, backoff: Option[FiniteDuration] = None) extends InputStream {
+class InsistentInputStream(streamBuilder: () => InputStream, maxRetries: Int = 10, backoff: Option[FiniteDuration] = None) extends InputStream with Logging {
 
   private[this] var innerStream: InputStream = streamBuilder()
   private[this] var currPos: Long = 0
@@ -36,21 +38,37 @@ class InsistentInputStream(streamBuilder: () => InputStream, maxRetries: Int = 1
   }
 
   @tailrec
-  private[this] def readRetries(remainingTries: Int): Int = {
-    Try(innerStream.read) match {
+  private[this] def readRetries(remainingTries: Int, f: => Int): Int = {
+    Try(f) match {
       case Success(n) =>
-        currPos += 1
         n
       case Failure(t) =>
         if (remainingTries <= 0) throw t
         else {
+          log.warn("Failed to read from stream: {}", t)
           val nextRemainingTries = retryStreamCreation(remainingTries - 1)
-          readRetries(nextRemainingTries)
+          readRetries(nextRemainingTries, f)
         }
     }
   }
 
-  def read(): Int = readRetries(maxRetries)
+  def read(): Int = {
+    val nextByte = readRetries(maxRetries, innerStream.read())
+    if (nextByte > 0) currPos += 1
+    nextByte
+  }
+
+  override def read(b: Array[Byte]): Int = {
+    val bytesRead = readRetries(maxRetries, innerStream.read(b))
+    if (bytesRead > 0) currPos += bytesRead
+    bytesRead
+  }
+
+  override def read(b: Array[Byte], off: Int, len: Int): Int = {
+    val bytesRead = readRetries(maxRetries, innerStream.read(b, off, len))
+    if (bytesRead > 0) currPos += bytesRead
+    bytesRead
+  }
 
   override def available(): Int = innerStream.available()
   override def close() = innerStream.close()
--- apso/src/test/scala/eu/shiftforward/apso/io/InsistentInputStreamSpec.scala
+++ apso/src/test/scala/eu/shiftforward/apso/io/InsistentInputStreamSpec.scala
@@ -8,7 +8,7 @@ class InsistentInputStreamSpec extends Specification {
 
   "An InsistentInputStream" should {
 
-    val testList = List[Byte](1, 2, 3, 4, 5)
+    val testList = (1 to 100).map(_.toByte)
 
     def testGoodInputStream() = new InputStream {
       val iter = testList.iterator
@@ -19,9 +19,10 @@ class InsistentInputStreamSpec extends Specification {
       var fail = false
       val iter = testList.iterator
       def read(): Int = {
-        if (fail) throw new Exception("bad read")
-        else {
-          fail = true
+        if (fail) {
+          fail = false
+          throw new Exception("bad read")
+        } else {
           iter.next()
         }
       }
@@ -29,6 +30,7 @@ class InsistentInputStreamSpec extends Specification {
         iter.drop(l.toInt)
         l
       }
+      def failNext() = fail = true
     }
 
     def testBadInputStream() = new InputStream {
@@ -39,25 +41,45 @@ class InsistentInputStreamSpec extends Specification {
     "have a working input stream interface" in {
 
       val stream = new InsistentInputStream(testGoodInputStream)
+      val testBuffer = Array[Byte](0, 0, 0)
 
       stream.read() === 1
       stream.read() === 2
       stream.skip(2) === 2
       stream.read() === 5
+      stream.read(testBuffer) === 3
+      testBuffer === Array[Byte](6, 7, 8)
+      stream.skip(1) === 1
+      stream.read() === 10
+      stream.read(testBuffer, 1, 2) === 2
+      testBuffer === Array[Byte](6, 11, 12)
     }
 
     "retry reads when a inner stream is buggy" in {
       val buggyStream = testBuggyInputStream()
 
       buggyStream.read() === 1
+      buggyStream.failNext()
       buggyStream.read() must throwAn[Exception]
 
       val stream = new InsistentInputStream(testBuggyInputStream)
+      val testBuffer = Array[Byte](0, 0, 0)
 
       stream.read() === 1
+      buggyStream.failNext()
       stream.read() === 2
       stream.skip(2) === 2
+      buggyStream.failNext()
       stream.read() === 5
+      buggyStream.failNext()
+      stream.read(testBuffer) === 3
+      testBuffer === Array[Byte](6, 7, 8)
+      stream.skip(1) === 1
+      buggyStream.failNext()
+      stream.read() === 10
+      buggyStream.failNext()
+      stream.read(testBuffer, 1, 2) === 2
+      testBuffer === Array[Byte](6, 11, 12)
     }
 
     "fail after the retry limit is exceeded" in {
