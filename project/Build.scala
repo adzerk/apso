98e58a51c63c110e9e13789b00de4a2327d231f0
Rui Gonçalves <ruippeixotog@gmail.com>
2016-11-02 12:34:28 +0000

    Bump apso version

--- project/Build.scala
+++ project/Build.scala
@@ -59,7 +59,7 @@ object ProjectBuild extends Build {
 
   lazy val commonSettings = Defaults.coreDefaultSettings ++ formatSettings ++ Seq(
     organization := "eu.shiftforward",
-    version := "0.9.4",
+    version := "0.9.5",
     scalaVersion := "2.11.8",
 
     resolvers ++= Seq(
