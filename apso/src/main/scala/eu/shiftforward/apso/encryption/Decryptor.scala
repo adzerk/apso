17219a62abd62286c9f9b9dde5f2964a3abf37b9
André Cardoso <andre.cardoso@shiftforward.eu>
2017-01-08 15:08:29 +0000

    Add encryption utils

---
+++ apso/src/main/scala/eu/shiftforward/apso/encryption/Decryptor.scala
@@ -0,0 +1,63 @@
+package eu.shiftforward.apso.encryption
+
+import java.io.InputStream
+import java.security.{ Key, MessageDigest }
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+
+import org.apache.commons.codec.binary.Base64
+
+import eu.shiftforward.apso.Logging
+
+class Decryptor(decryptor: Cipher) extends EncryptionErrorHandling {
+  def apply(s: String): Option[String] = decrypt(s)
+
+  def decrypt(s: String, base64: Boolean = true): Option[String] = handle {
+    if (base64) EncryptionUtils.paddedUrlSafebase64(decryptor.doFinal(Base64.decodeBase64(s)))
+    else new String(decryptor.doFinal(Base64.decodeBase64(s)))
+  }
+
+  def decryptToString(s: String): Option[String] = handle {
+    new String(decryptor.doFinal(Base64.decodeBase64(s)))
+  }
+}
+
+object Decryptor extends EncryptionUtils with Logging {
+
+  private def loadDecryptionCipher(transformation: String, key: Key): Option[Cipher] = handle(
+    {
+      log.debug(s"Building Decryptor using Transformation '$transformation' and Key Algorithm '${key.getAlgorithm}'")
+      val cipher = Cipher.getInstance(transformation, provider)
+      cipher.init(Cipher.DECRYPT_MODE, key)
+      cipher
+    },
+    {
+      ex: Throwable =>
+        log.warn(s"Cipher Transformation: $transformation")
+        log.warn("Cipher Key: " + key)
+        log.warn(s"Impossible to create Decryption Cipher!", ex)
+    })
+
+  def apply(transformation: String, key: Array[Byte]): Option[Decryptor] =
+    loadDecryptionCipher(transformation, new SecretKeySpec(key, transformation)).map(new Decryptor(_))
+
+  def apply(transformation: String, key: String): Option[Decryptor] = {
+    val md = MessageDigest.getInstance("SHA-256")
+
+    loadDecryptionCipher(
+      transformation,
+      new SecretKeySpec(md.digest(key.getBytes("UTF-8")), transformation)).map(new Decryptor(_))
+  }
+
+  def apply(
+    transformation: String,
+    key: InputStream,
+    keyStorePassword: String,
+    keyAlias: String,
+    keyPassword: String): Option[Decryptor] =
+    for {
+      keyStore <- loadKeyStore(key, keyStorePassword)
+      key <- getKey(keyStore, keyAlias, keyPassword)
+      cipher <- loadDecryptionCipher(transformation, key)
+    } yield new Decryptor(cipher)
+}
