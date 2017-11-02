a79ba4189b72e66262b996e4f5d4104828b03edf
André Cardoso <andre.cardoso@shiftforward.eu>
2017-11-02 12:31:56 +0000

    Add new retry for simple blocking functions

--- core/src/main/scala/eu/shiftforward/apso/Retry.scala
+++ core/src/main/scala/eu/shiftforward/apso/Retry.scala
@@ -1,9 +1,11 @@
 package eu.shiftforward.apso
 
+import scala.concurrent.duration._
 import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.{ Failure, Success, Try }
 
 /**
- * Utility object for retrying Future a number of times.
+ * Utility object with retry mechanisms.
  */
 object Retry {
 
@@ -17,6 +19,7 @@ object Retry {
    * @tparam T the type of what the future completes with
    * @return the resulting `f` function
    */
+  @deprecated("This will be removed in a future version. Please use `retryFuture()` instead", "2017/10/31")
   def apply[T](
     maxRetries: Int = 10,
     inBetweenSleep: Option[Long] = Some(100))(f: => Future[T])(implicit ec: ExecutionContext): Future[T] = {
@@ -28,7 +31,56 @@ object Retry {
         f recoverWith {
           case _ =>
             inBetweenSleep.foreach(Thread.sleep)
-            apply(maxRetries - 1, inBetweenSleep)(f)
+            apply[T](maxRetries - 1, inBetweenSleep)(f)
+        }
+    }
+  }
+
+  /**
+   * Tries to perform a Future[T] until it succeeds or until maximum retries is reached.
+   *
+   * @param maxRetries the number of retries, 10 by default
+   * @param inBetweenSleep the duration to wait between attempts, 100 milliseconds by default
+   * @param f the function that returns the Future[T]
+   * @param ec the implicit execution context
+   * @tparam T the type of what the future completes with
+   * @return the Future[T]
+   */
+  def retryFuture[T](
+    maxRetries: Int = 10,
+    inBetweenSleep: Option[FiniteDuration] = Some(100.millis))(f: => Future[T])(implicit ec: ExecutionContext): Future[T] = {
+    maxRetries match {
+      case 0 =>
+        f
+      case _ =>
+        f recoverWith {
+          case _: Throwable =>
+            inBetweenSleep.foreach(d => Thread.sleep(d.toMillis))
+            retryFuture[T](maxRetries - 1, inBetweenSleep)(f)
+        }
+    }
+  }
+
+  /**
+   * Tries to perform a function `f` until it succeeds or until maximum retries is reached.
+   *
+   * @param maxRetries the number of retries, 10 by default
+   * @param inBetweenSleep the duration to wait between attempts, 100 milliseconds by default
+   * @param f the function
+   * @tparam T the type of what the function returns
+   * @return a Try of the `f` function result
+   */
+  def retry[T](
+    maxRetries: Int = 10,
+    inBetweenSleep: Option[FiniteDuration] = Some(100.millis))(f: => T): Try[T] = {
+    maxRetries match {
+      case 0 => Try(f)
+      case _ =>
+        Try(f) match {
+          case res @ Success(_) => res
+          case Failure(_) =>
+            inBetweenSleep.foreach(d => Thread.sleep(d.toMillis))
+            retry[T](maxRetries - 1, inBetweenSleep)(f)
         }
     }
   }
