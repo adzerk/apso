b2bc4d4e3f8f0ef8440d0f4365f10dcbead86620
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2018-10-16 17:24:52 +0100

    Improve json tests with circe-literal

--- core/src/test/scala/com/velocidi/apso/json/ImplicitsSpec.scala
+++ core/src/test/scala/com/velocidi/apso/json/ImplicitsSpec.scala
@@ -5,7 +5,6 @@ import org.specs2.mutable._
 import io.circe.generic.semiauto._
 import io.circe.literal._
 import io.circe.syntax._
-import io.circe.parser._
 import spray.json.DefaultJsonProtocol._
 import spray.json._
 
@@ -62,7 +61,8 @@ class ImplicitsSpec extends Specification {
     }
 
     "provide a method to create a JSON object from complete paths" in {
-      val expected = """{ "a": {"b": {"c": 1, "d": {"e": 3}}, "f": 5}, "g": 4 }"""
+      val expectedJsValue = """{ "a": {"b": {"c": 1, "d": {"e": 3}}, "f": 5}, "g": 4 }""".parseJson
+      val expectedJson = json"""{ "a": {"b": {"c": 1, "d": {"e": 3}}, "f": 5}, "g": 4 }"""
 
       "create a spray-json JSON object from complete paths" in {
         val res = fromFullPaths(
@@ -72,7 +72,7 @@ class ImplicitsSpec extends Specification {
             "a.f" -> JsNumber(5),
             "g" -> JsNumber(4)))
 
-        res mustEqual expected.parseJson
+        res mustEqual expectedJsValue
       }
 
       "create a circe JSON object from complete paths" in {
@@ -83,7 +83,7 @@ class ImplicitsSpec extends Specification {
             "a.f" -> 5.asJson,
             "g" -> 4.asJson))
 
-        res mustEqual parse(expected).fold(throw _, identity)
+        res mustEqual expectedJson
       }
 
       "create a spray-json JSON object from complete paths (with a custom separator)" in {
@@ -94,7 +94,7 @@ class ImplicitsSpec extends Specification {
             "a-f" -> JsNumber(5),
             "g" -> JsNumber(4)), "-")
 
-        res mustEqual expected.parseJson
+        res mustEqual expectedJsValue
       }
 
       "create a circe JSON object from complete paths (with a custom separator)" in {
@@ -105,22 +105,20 @@ class ImplicitsSpec extends Specification {
             "a-f" -> 5.asJson,
             "g" -> 4.asJson), "-")
 
-        res mustEqual parse(expected).fold(throw _, identity)
+        res mustEqual expectedJson
       }
     }
 
     "provide a method to get the flattened key-value set of a JSON Object" in {
-      val jsonString = """{"a":1,"b":{"c":2}, "b.c": 3, "d":null}"""
-
       "for spray-json JSON objects" in {
-        val obj = jsonString.parseJson.asJsObject
+        val obj = """{"a":1,"b":{"c":2}, "b.c": 3, "d":null}""".parseJson.asJsObject
 
         obj.flattenedKeyValueSet(".") === Set("a" -> JsNumber(1), "b.c" -> JsNumber(2), "b.c" -> JsNumber(3), "d" -> JsNull)
         obj.flattenedKeyValueSet("/") === Set("a" -> JsNumber(1), "b/c" -> JsNumber(2), "b.c" -> JsNumber(3), "d" -> JsNull)
       }
 
       "for circe JSON objects" in {
-        val obj = parse(jsonString).fold(throw _, identity)
+        val obj = json"""{"a":1,"b":{"c":2}, "b.c": 3, "d":null}"""
 
         obj.flattenedKeyValueSet(".") === Set("a" -> Json.fromInt(1), "b.c" -> Json.fromInt(2), "b.c" -> Json.fromInt(3), "d" -> Json.Null)
         obj.flattenedKeyValueSet("/") === Set("a" -> Json.fromInt(1), "b/c" -> Json.fromInt(2), "b.c" -> Json.fromInt(3), "d" -> Json.Null)
@@ -129,10 +127,8 @@ class ImplicitsSpec extends Specification {
     }
 
     "provide a method to get the key set of a JSON Object" in {
-      val jsonString = """{"a":1,"b":{"c":2},"d":null}"""
-
       "for spray-json JSON objects" in {
-        val obj = jsonString.parseJson.asJsObject
+        val obj = """{"a":1,"b":{"c":2},"d":null}""".parseJson.asJsObject
 
         obj.flattenedKeySet(".", ignoreNull = true) === Set("a", "b.c")
         obj.flattenedKeySet(".", ignoreNull = false) === Set("a", "b.c", "d")
@@ -140,7 +136,7 @@ class ImplicitsSpec extends Specification {
       }
 
       "for circe JSON objects" in {
-        val obj = parse(jsonString).fold(throw _, identity)
+        val obj = json"""{"a":1,"b":{"c":2},"d":null}"""
 
         obj.flattenedKeySet(".", ignoreNull = true) === Set("a", "b.c")
         obj.flattenedKeySet(".", ignoreNull = false) === Set("a", "b.c", "d")
@@ -150,7 +146,7 @@ class ImplicitsSpec extends Specification {
     }
 
     "provide a method to get a field from a JSON object" in {
-      val obj = parse("""{"a":"abc","b":{"c":2},"d":null}""").fold(throw _, identity)
+      val obj = json"""{"a":"abc","b":{"c":2},"d":null}"""
 
       obj.getField[Int]("b.c") must beSome(2)
       obj.getField[Int]("b,c", ',') must beSome(2)
