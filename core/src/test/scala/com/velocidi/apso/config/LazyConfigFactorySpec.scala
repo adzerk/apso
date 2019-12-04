fe345b634eb80415be119c9a3ff91a6b420bcc4b
Bruno Maia <bruno.mm.maia@gmail.com>
2019-12-04 15:46:35 +0000

    Remove typesafe config tests as the updated version behaves differently

--- core/src/test/scala/com/velocidi/apso/config/LazyConfigFactorySpec.scala
+++ core/src/test/scala/com/velocidi/apso/config/LazyConfigFactorySpec.scala
@@ -1,6 +1,5 @@
 package com.velocidi.apso.config
 
-import com.typesafe.config.ConfigFactory
 import org.specs2.mutable.Specification
 
 class LazyConfigFactorySpec extends Specification {
@@ -9,17 +8,14 @@ class LazyConfigFactorySpec extends Specification {
   "A LazyConfigLoader" should {
 
     "Resolve variables in reference.conf only when application.conf is included" in {
-      ConfigFactory.load.getString("host2") mustEqual "localhost"
       LazyConfigFactory.load.getString("host2") mustEqual "192.168.59.103"
     }
 
     "Resolve variables in reference.conf only when a custom config is included" in {
-      ConfigFactory.load("test").getString("host2") mustEqual "localhost"
       LazyConfigFactory.load("test").getString("host2") mustEqual "192.168.59.104"
     }
 
     "Load an overrides.conf that has priority over application.conf if it exists" in {
-      ConfigFactory.load.getString("path2") mustEqual "."
       LazyConfigFactory.load.getString("path2") mustEqual System.getenv.get("PATH")
     }
   }
