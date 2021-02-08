229ff3c96066eb375bff88d01813f60efec0cee0
André Cardoso <thyandrecardoso@gmail.com>
2021-02-08 17:52:40 +0000

    Split example in 2

--- elasticsearch/src/test/scala/com/velocidi/apso/elasticsearch/ElasticsearchBulkInserterSpec.scala
+++ elasticsearch/src/test/scala/com/velocidi/apso/elasticsearch/ElasticsearchBulkInserterSpec.scala
@@ -116,60 +116,62 @@ class ElasticsearchBulkInserterSpec(implicit ee: ExecutionEnv) extends AkkaSpeci
     }
 
     "correctly send error back to client only when retries are exhausted" in {
+      "when the buffer maximum size is reached" in {
+        val msgIndex = "test-index-5"
+        // maxTryCount is 3
+        val bulkInserter = testBulkInserter(maxBufferSize = 2, flushFreq = 1.day)
 
-      // NOTICE: trying on a setting in which the retries are triggered by the buffer reaching its max capacity
-      val msgIndex1 = "test-index-5"
-      // maxTryCount is 3
-      val bulkInserter1 = testBulkInserter(maxBufferSize = 2, flushFreq = 1.day)
+        // use a mapping that does not allow for extra fields other than the "name" one
+        esClient.execute(putMapping(msgIndex) rawSource """{"dynamic":"strict","properties":{"name":{"type":"text"}}}""") must
+          beAnInstanceOf[RequestSuccess[_]].awaitFor(5.seconds)
 
-      // use a mapping that does not allow for extra fields other than the "name" one
-      esClient.execute(putMapping(msgIndex1) rawSource """{"dynamic":"strict","properties":{"name":{"type":"text"}}}""") must
-        beAnInstanceOf[RequestSuccess[_]].awaitFor(5.seconds)
+        // insert valid documents
+        bulkInserter ! Insert(Json.obj("name" := "test1"), msgIndex)
+        bulkInserter ! Insert(Json.obj("name" := "test2"), msgIndex)
+        numberOfHits(msgIndex) must be_==(2).retry(eventuallyRetries = 30)
 
-      // insert valid documents
-      bulkInserter1 ! Insert(Json.obj("name" := "test1"), msgIndex1)
-      bulkInserter1 ! Insert(Json.obj("name" := "test2"), msgIndex1)
-      numberOfHits(msgIndex1) must be_==(2).retry(eventuallyRetries = 30)
+        // insert an invalid document that will be retried
+        val probe = TestProbe()
+        bulkInserter.!(Insert(Json.obj("name" := "test3", "other" := "dynamic_field_value"), msgIndex))(probe.ref)
+        numberOfHits(msgIndex) must be_==(2).retry(eventuallyRetries = 30)
+        probe must not(receive.like { case Status.Failure(_) => ok })
 
-      // insert an invalid document that will be retried
-      val probe = TestProbe()
-      bulkInserter1.!(Insert(Json.obj("name" := "test3", "other" := "dynamic_field_value"), msgIndex1))(probe.ref)
-      numberOfHits(msgIndex1) must be_==(2).retry(eventuallyRetries = 30)
-      probe must not(receive.like { case Status.Failure(_) => ok })
+        // after this valid one, the failed one above will be retried...
+        bulkInserter ! Insert(Json.obj("name" := "test4"), msgIndex)
+        numberOfHits(msgIndex) must be_==(3).retry(eventuallyRetries = 30)
+        probe must not(receive.like { case Status.Failure(_) => ok })
 
-      // after this valid one, the failed one above will be retried...
-      bulkInserter1 ! Insert(Json.obj("name" := "test4"), msgIndex1)
-      numberOfHits(msgIndex1) must be_==(3).retry(eventuallyRetries = 30)
-      probe must not(receive.like { case Status.Failure(_) => ok })
+        // after this valid one, the failed one above will be retried...
+        bulkInserter ! Insert(Json.obj("name" := "test5"), msgIndex)
+        numberOfHits(msgIndex) must be_==(4).retry(eventuallyRetries = 30)
+        probe must not(receive.like { case Status.Failure(_) => ok })
 
-      // after this valid one, the failed one above will be retried...
-      bulkInserter1 ! Insert(Json.obj("name" := "test5"), msgIndex1)
-      numberOfHits(msgIndex1) must be_==(4).retry(eventuallyRetries = 30)
-      probe must not(receive.like { case Status.Failure(_) => ok })
+        // after this valid one, the failed one above will be retried...
+        bulkInserter ! Insert(Json.obj("name" := "test6"), msgIndex)
+        numberOfHits(msgIndex) must be_==(5).retry(eventuallyRetries = 30)
+        probe must not(receive.like { case Status.Failure(_) => ok })
 
-      // after this valid one, the failed one above will be retried...
-      bulkInserter1 ! Insert(Json.obj("name" := "test6"), msgIndex1)
-      numberOfHits(msgIndex1) must be_==(5).retry(eventuallyRetries = 30)
-      probe must not(receive.like { case Status.Failure(_) => ok })
+        // after this valid one, finally the error will be sent back to the client
+        bulkInserter ! Insert(Json.obj("name" := "test7"), msgIndex)
+        numberOfHits(msgIndex) must be_==(6).retry(eventuallyRetries = 30)
+        probe must receive.like { case Status.Failure(_) => ok }
+      }
 
-      // after this valid one, finally the error will be sent back to the client
-      bulkInserter1 ! Insert(Json.obj("name" := "test7"), msgIndex1)
-      numberOfHits(msgIndex1) must be_==(6).retry(eventuallyRetries = 30)
-      probe must receive.like { case Status.Failure(_) => ok }
+      "when the buffer is automatically flushed on schedule" in {
+        val msgIndex = "test-index-6"
+        // maxTryCount is 3
+        val bulkInserter = testBulkInserter(maxBufferSize = 2, flushFreq = 500.millis)
 
-      // NOTICE: trying on a setting in which the retries are triggered by the flush frequency
-      val msgIndex2 = "test-index-6"
-      // maxTryCount is 3
-      val bulkInserter2 = testBulkInserter(maxBufferSize = 2, flushFreq = 500.millis)
+        // use a mapping that does not allow for extra fields other than the "name" one
+        esClient.execute(putMapping(msgIndex) rawSource """{"dynamic":"strict","properties":{"name":{"type":"text"}}}""") must
+          beAnInstanceOf[RequestSuccess[_]].awaitFor(5.seconds)
 
-      // use a mapping that does not allow for extra fields other than the "name" one
-      esClient.execute(putMapping(msgIndex2) rawSource """{"dynamic":"strict","properties":{"name":{"type":"text"}}}""") must
-        beAnInstanceOf[RequestSuccess[_]].awaitFor(5.seconds)
-
-      // insert an invalid document that will be retried
-      bulkInserter2.!(Insert(Json.obj("name" := "test3", "other" := "dynamic_field_value"), msgIndex2))(probe.ref)
-      numberOfHits(msgIndex2) must be_==(0).retry(eventuallyRetries = 30)
-      probe must receive.like { case Status.Failure(_) => ok }.eventually(30, 2.seconds)
+        // insert an invalid document that will be retried
+        val probe = TestProbe()
+        bulkInserter.!(Insert(Json.obj("name" := "test3", "other" := "dynamic_field_value"), msgIndex))(probe.ref)
+        numberOfHits(msgIndex) must be_==(0).retry(eventuallyRetries = 30)
+        probe must receive.like { case Status.Failure(_) => ok }.eventually(30, 2.seconds)
+      }
     }
   }
 
