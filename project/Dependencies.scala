2d3f894d24f08f569888a9bb958abb5ecb26bfb9
Scala Steward <scala_steward@virtuslab.com>
2022-09-07 12:09:55 +0000

    Update akka-actor, ... to 2.6.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val Akka                    = "2.6.19"
+    val Akka                    = "2.6.20"
     val AkkaHttp                = "10.2.9"
     val AkkaTestkitSpecs2       = "0.3.1"
     val Aws                     = "1.12.292"
