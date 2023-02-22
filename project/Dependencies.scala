12294f5af278331ed280dbd227932caa32763f6e
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-02-22 13:47:30 +0000

    Bump elasticsearch-cluster-runner to latest within major v7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -58,7 +58,7 @@ object Dependencies {
   val Elastic4sCore              = "com.sksamuel.elastic4s"     %% "elastic4s-core"               % Versions.Elastic4s
   val Elastic4sTestkit           = "com.sksamuel.elastic4s"     %% "elastic4s-testkit"            % Versions.Elastic4s
   val Elasticsearch              = "org.elasticsearch"           % "elasticsearch"                % "7.17.9"
-  val ElasticsearchClusterRunner = "org.codelibs"                % "elasticsearch-cluster-runner" % "7.10.2.0"
+  val ElasticsearchClusterRunner = "org.codelibs"                % "elasticsearch-cluster-runner" % "7.17.1.0"
   val ElasticsearchRestClient    = "org.elasticsearch.client"    % "elasticsearch-rest-client"    % "7.17.9"
   val FastMd5                    = "com.joyent.util"             % "fast-md5"                     % Versions.FastMd5
   val JodaTime                   = "joda-time"                   % "joda-time"                    % Versions.JodaTime
