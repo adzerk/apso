b1a8d442ef2795f1bd935d4ac2ae17114cfe634f
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-03-17 12:02:30 +0000

    Rename SFTP file descriptor credentials class

--- io/src/main/scala/com/velocidi/apso/io/SftpFileDescriptor.scala
+++ io/src/main/scala/com/velocidi/apso/io/SftpFileDescriptor.scala
@@ -240,7 +240,7 @@ object SftpFileDescriptor {
     }
   }
 
-  case class SftpFileDescriptorCredentials(host: String, port: String, auth: Either[Identity, String])
+  case class Credentials(host: String, port: String, auth: Either[Identity, String])
 
   implicit def sftpClientToSFTPClient(c: SftpClient): SFTPClient = c.sftpClient
 
@@ -328,18 +328,18 @@ object SftpFileDescriptor {
   }
 
   private[this] val credentials =
-    new FileDescriptorCredentials[config.Credentials.Sftp.Entry, SftpFileDescriptorCredentials] {
+    new FileDescriptorCredentials[config.Credentials.Sftp.Entry, SftpFileDescriptor.Credentials] {
       def id(path: String) = splitMeta(path)._1
 
       def createCredentials(
           hostname: String,
           sftpConfig: config.Credentials.Sftp.Entry
-      ): SftpFileDescriptorCredentials = {
+      ): SftpFileDescriptor.Credentials = {
         sftpConfig match {
           case config.Credentials.Sftp.Entry.Basic(username, password) =>
-            SftpFileDescriptorCredentials(hostname, username, Right(password))
+            SftpFileDescriptor.Credentials(hostname, username, Right(password))
           case config.Credentials.Sftp.Entry.PublicKey(username, keypairFile, passphrase) =>
-            SftpFileDescriptorCredentials(
+            SftpFileDescriptor.Credentials(
               hostname,
               username,
               Left(Identity(Right(new File(Properties.userHome + "/.ssh/" + keypairFile)), passphrase))
@@ -376,14 +376,14 @@ object SftpFileDescriptor {
     apply(host, port, url, username, password, None)
   }
 
-  def apply(url: String, credentials: Option[SftpFileDescriptorCredentials]): SftpFileDescriptor = {
+  def apply(url: String, credentials: Option[SftpFileDescriptor.Credentials]): SftpFileDescriptor = {
     val creds = credentials.getOrElse(throw new IllegalArgumentException(s"No credentials found."))
     val (_, port, _) = splitMeta(url)
 
     creds match {
-      case SftpFileDescriptorCredentials(host, username, Left(identity)) =>
+      case SftpFileDescriptor.Credentials(host, username, Left(identity)) =>
         apply(host, port, url, username, None, Some(identity))
-      case SftpFileDescriptorCredentials(host, username, Right(password)) =>
+      case SftpFileDescriptor.Credentials(host, username, Right(password)) =>
         apply(host, port, url, username, Some(password), None)
     }
   }
