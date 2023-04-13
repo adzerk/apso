dc78565a038f46e1184e4959cdccc999a05372c0
Scala Steward <scala_steward@virtuslab.com>
2023-04-13 12:12:57 +0000

    Update bcpkix-jdk18on, bcprov-jdk18on to 1.73

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val AkkaHttp                = "10.2.10"
     val AkkaTestkitSpecs2       = "0.3.2"
     val Aws                     = "1.12.445"
-    val BouncyCastle            = "1.72"
+    val BouncyCastle            = "1.73"
     val Cats                    = "2.9.0"
     val Circe                   = "0.14.5"
     val CommonsCodec            = "1.15"
