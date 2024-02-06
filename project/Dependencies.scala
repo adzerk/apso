d4b3914a393da332c0a0c9d492e18cb91a9c330e
Scala Steward <scala_steward@virtuslab.com>
2024-02-06 13:14:36 +0000

    Update aws-java-sdk-core, aws-java-sdk-s3 to 1.12.652

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val Akka                    = "2.6.20"
     val AkkaHttp                = "10.2.10"
     val AkkaTestkitSpecs2       = "0.3.2"
-    val Aws                     = "1.12.649"
+    val Aws                     = "1.12.652"
     val BouncyCastle            = "1.77"
     val Cats                    = "2.10.0"
     val Circe                   = "0.14.6"
