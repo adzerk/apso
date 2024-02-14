3b05bf1532da311076b5e60e1d6d7a0a7f732ee5
pmscosta <pcosta@kevel.co>
2024-02-14 17:23:27 +0000

    Correctly use isRetryable flag on exception handler

--- aws/src/main/scala/com/velocidi/apso/aws/S3Bucket.scala
+++ aws/src/main/scala/com/velocidi/apso/aws/S3Bucket.scala
@@ -332,7 +332,7 @@ class S3Bucket(
       }
 
     case ex: AmazonServiceException =>
-      logger.error(s"Service error: ${ex.getMessage}", ex); ex.isRetryable
+      logger.error(s"Service error: ${ex.getMessage}", ex); !ex.isRetryable
 
     case ex: AmazonClientException
         if ex.getMessage ==
@@ -340,7 +340,7 @@ class S3Bucket(
       logger.error("Unable to load AWS credentials", ex); true
 
     case ex: AmazonClientException =>
-      logger.error("Client error pulling file", ex); ex.isRetryable
+      logger.error("Client error pulling file", ex); !ex.isRetryable
 
     case ex: Exception =>
       logger.error("An error occurred", ex); false
