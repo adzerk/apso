4d90647f325142edfee3eb26c6f0c4a769025163
Scala Steward <scala_steward@virtuslab.com>
2024-09-30 12:51:56 +0000

    Update log4j-core, log4j-slf4j-impl to 2.24.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val FastMd5                 = "2.7.1"
     val JUnit                   = "4.13.2"
     val JodaTime                = "2.13.0"
-    val Log4J                   = "2.24.0"
+    val Log4J                   = "2.24.1"
     val NscalaTime              = "2.32.0"
     val ScalaCheck              = "1.18.1"
     val ScalaLogging            = "3.9.5"
