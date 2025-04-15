1cc94184d817f0f7e3a22d305c966a3f7afc9a5d
Bruno Mendes <bd_mendes@outlook.com>
2025-04-15 14:02:09 +0100

    Document SyncEvidence

--- caching/src/main/scala/com/kevel/apso/caching/SyncEvidence.scala
+++ caching/src/main/scala/com/kevel/apso/caching/SyncEvidence.scala
@@ -2,6 +2,13 @@ package com.kevel.apso.caching
 
 import scala.concurrent.Future
 
+/** An evidence that an `A` is not a `Future[_]`. For this we use a trick similar to Shapeless `=!=` operator, i.e. we
+  * induce an ambiguous implicit resolution for any `Future[_]`, with higher priority than the properly defined single
+  * instance for any `A`. These implicits are available wherever the type is available.
+  *
+  * @tparam A
+  *   the tagged type.
+  */
 private[caching] trait SyncEvidence[A]
 
 private[caching] object SyncEvidence extends LowPrioritySyncEvidence {
