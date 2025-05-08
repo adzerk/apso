3f36b83849fc310ed043a22267a6b3f0c748c1b9
Scala Steward <scala_steward@virtuslab.com>
2025-05-08 21:23:40 +0000

    Update scala3-library to 3.3.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val Scala213 = "2.13.16"
-    val Scala3   = "3.3.5"
+    val Scala3   = "3.3.6"
 
     val Pekko          = "1.1.3"
     val PekkoHttp      = "1.1.0"
