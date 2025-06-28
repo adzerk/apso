6fccceedbb2b0f5b4da702ad243cd98deffd5a15
Beatriz Magalhães <bmagalhaes@kevel.com>
2025-06-28 15:35:26 +0100

    Replace usages of implicit with using

--- testkit-specs2-5/src/main/scala/com/kevel/apso/JceTestHelper.scala
+++ testkit-specs2-5/src/main/scala/com/kevel/apso/JceTestHelper.scala
@@ -8,7 +8,7 @@ object JceTestHelper {
 
   /** Ensures the Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files are installed.
     */
-  def unlimitedJCE[T](r: => T)(implicit evidence: AsResult[T]): Result = {
+  def unlimitedJCE[T](r: => T)(using evidence: AsResult[T]): Result = {
     if (Cipher.getMaxAllowedKeyLength("sha256") == Int.MaxValue) evidence.asResult(r)
     else Skipped("Java Cryptography Extension Unlimited Strength Jurisdiction Policy Files not installed.")
   }
