ebcc59de6a70812954d5d2c3edfe5824f76e8a4e
Scala Steward <scala_steward@virtuslab.com>
2025-11-06 01:38:37 +0000

    Update commons-codec to 1.20.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val BouncyCastle   = "1.82"
     val Cats           = "2.13.0"
     val Circe          = "0.14.15"
-    val CommonsCodec   = "1.19.0"
+    val CommonsCodec   = "1.20.0"
     val FastMd5        = "2.7.1"
     val Gcp            = "2.59.0"
     val JUnit          = "4.13.2"
