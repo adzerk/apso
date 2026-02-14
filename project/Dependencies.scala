b39bf61b7f30278f27ac6312f65a38882bfe3b73
Scala Steward <scala_steward@virtuslab.com>
2026-02-14 14:55:13 +0000

    Update aws-crt to 0.43.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val Pekko          = "1.4.0"
     val PekkoHttp      = "1.3.0"
     val Aws            = "2.41.25"
-    val AwsCrt         = "0.43.1"
+    val AwsCrt         = "0.43.3"
     val BouncyCastle   = "1.83"
     val Cats           = "2.13.0"
     val Circe          = "0.14.15"
