28cc4be85889cb4744ea04cd396d7746b5544ea1
Scala Steward <scala_steward@virtuslab.com>
2026-03-20 14:44:52 +0000

    Update unirest-java-core to 4.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
     val Squants        = "1.8.3"
     val SshJ           = "0.40.0"
     val TypesafeConfig = "1.4.6"
-    val UnirestJava    = "4.7.4"
+    val UnirestJava    = "4.8.0"
   }
 
   // scalafmt: { maxColumn = 200 }
