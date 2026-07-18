91d1e6c4f57d8cd3011f34871527313b429f2cc7
Bruno Mendes <bd_mendes@outlook.com>
2026-07-18 11:31:26 +0100

    Make SyncEvidence a public class

--- modules/caching/src/main/scala-3/com/kevel/apso/caching/SyncEvidence.scala
+++ modules/caching/src/main/scala-3/com/kevel/apso/caching/SyncEvidence.scala
@@ -3,13 +3,14 @@ package com.kevel.apso.caching
 import scala.concurrent.Future
 import scala.util.NotGiven
 
-/** An evidence that an `A` is not a `Future[_]`.
+/** An evidence that an `A` is not a `Future[?]`. This class and respective companion object are public since they are
+  * required by public methods, but they should not be used explicitly in user code.
   *
   * @tparam A
   *   the tagged type.
   */
-private[caching] trait SyncEvidence[A]
+class SyncEvidence[A]
 
-private[caching] object SyncEvidence {
-  implicit def ev[A](implicit notFuture: NotGiven[A <:< Future[_]]): SyncEvidence[A] = new SyncEvidence[A] {}
+object SyncEvidence {
+  given [A](using NotGiven[A <:< Future[?]]): SyncEvidence[A] = SyncEvidence[A]
 }
