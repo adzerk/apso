f0c8063cb2040768957d73e231972cabb9fda411
Scala Steward <scala_steward@virtuslab.com>
2026-08-21 13:23:10 +0000

    Update pekko-actor, ... to 1.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val Scala213 = "2.13.18"
     val Scala3   = "3.3.8"
 
-    val Pekko          = "1.6.0"
+    val Pekko          = "1.7.0"
     val PekkoHttp      = "1.4.0"
     val Aws            = "2.53.3"
     val AwsCrt         = "0.48.4"
